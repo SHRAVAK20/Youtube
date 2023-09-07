b0a1dee71acf8c8c0eaeb68b57d63d223e93302e
SRK <choudharyshravak@gmail.com>
2023-09-07 11:38:31 +0530

    Redux for hamburger to expand and collapse sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Body from './components/Body'
 import Head from './components/Head'
+import { Provider } from 'react-redux'
+import store from './utils/store'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <Provider store={store}>
+    <div>
       <Head/>
       <Body/>
-    </>
+    </div>
+    </Provider>
   )
 }
 
