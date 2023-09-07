3e1d6e60fb5fb06b9dd7bbad22195602270d9a6d
SRK <choudharyshravak@gmail.com>
2023-09-07 10:52:02 +0530

    Basic look of Youtube

---
+++ src/components/Body.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sidebar from './Sidebar'
+import MainContiner from './MainContiner'
+
+const Body = () => {
+  return (
+    <div className="flex">
+        <Sidebar/>
+        <MainContiner/>
+      
+    </div>
+  )
+}
+
+export default Body
