f6d8f4d53c2dec830e2a14a583d7ac74a110d210
SRK <choudharyshravak@gmail.com>
2023-09-07 11:59:00 +0530

    creating button in buttonlist

--- src/components/ButtonList.jsx
+++ src/components/ButtonList.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Button from './Button'
+
+const list = ["All", "Cricket", "Soccer", "News", "Live", "Songs", "Trending"]
 
 const ButtonList = () => {
   return (
-    <div>
-      ButtonList
+    <div className='flex'>
+      {list.map((new_name) => <Button name={new_name}/>)}
     </div>
   )
 }
